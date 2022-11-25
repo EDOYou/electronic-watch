48ffbde00f17b1f7adc671fd9627d4282c2a0c1f
Taghiyev <ktaghiyev01@gmail.com>
2022-11-25 22:38:58 +0100

    electronic-watch

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int seconds = scanner.nextInt();
+        int in = scanner.nextInt();
 
-
+        int secs, mins, hours;
+        hours = in / 3600 % 24;
+        mins = in % 3600 / 60;
+        secs = in % 3600 % 60;
+        System.out.printf("%d:%02d:%02d", hours, mins, secs);
     }
 }
